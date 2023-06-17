9cc0d7d2616d4b744c99f56c175af1e94a864657
adamnemecek <adamnemecek@gmail.com>
2023-06-17 11:16:52 -0700

    stuff

--- src/rule.rs
+++ src/rule.rs
@@ -14,63 +14,66 @@ pub struct Rule<T> {
     equiv: bool,
 }
 
-// impl<T> Rule<T> {
-//     pub fn new(
-//         lhs: Graph<T>,
-//         rhs: Graph<T>,
-//         name: &str,
-//         equiv: bool,
-//     ) -> Result<Self, RuleError> {
-//         if lhs.inputs().len() != rhs.inputs().len()
-//             || lhs.outputs().len() != rhs.outputs().len()
-//         {
-//             return Err(RuleError);
-//         }
-//         Ok(Self {
-//             lhs,
-//             rhs,
-//             name: String::from(name),
-//             equiv,
-//         })
-//     }
+impl<T> Rule<T> {
+    pub fn new(
+        lhs: Graph<T>,
+        rhs: Graph<T>,
+        name: &str,
+        equiv: bool,
+    ) -> Result<Self, RuleError> {
+        unimplemented!()
+        // if lhs.inputs().len() != rhs.inputs().len()
+        //     || lhs.outputs().len() != rhs.outputs().len()
+        // {
+        //     return Err(RuleError);
+        // }
+        // Ok(Self {
+        //     lhs,
+        //     rhs,
+        //     name: String::from(name),
+        //     equiv,
+        // })
+    }
 
-//     // pub fn copy(&self) -> Self {
-//     //     Self {
-//     //         lhs: self.lhs.copy(),
-//     //         rhs: self.rhs.copy(),
-//     //         name: self.name.clone(),
-//     //         equiv: self.equiv,
-//     //     }
-//     // }
+    // pub fn copy(&self) -> Self {
+    //     Self {
+    //         lhs: self.lhs.copy(),
+    //         rhs: self.rhs.copy(),
+    //         name: self.name.clone(),
+    //         equiv: self.equiv,
+    //     }
+    // }
 
-//     pub fn converse(&self) -> Self {
-//         let name = if self.name.starts_with('-') {
-//             self.name[1..].to_string()
-//         } else {
-//             format!("-{}", self.name)
-//         };
+    pub fn converse(&self) -> Self {
+        let name = if self.name.starts_with('-') {
+            self.name[1..].to_string()
+        } else {
+            format!("-{}", self.name)
+        };
 
-//         Self {
-//             lhs: self.rhs.copy(),
-//             rhs: self.lhs.copy(),
-//             name,
-//             equiv: true,
-//         }
-//     }
+        // Self {
+        //     lhs: self.rhs.copy(),
+        //     rhs: self.lhs.copy(),
+        //     name,
+        //     equiv: true,
+        // }
+        unimplemented!()
+    }
 
-//     pub fn is_left_linear(&self) -> bool {
-//         let mut verts = HashSet::new();
-//         let inputs = HashSet::from_iter(self.lhs.inputs());
-//         let outputs =
-//             HashSet::from_iter(self.lhs.outputs());
+    pub fn is_left_linear(&self) -> bool {
+        // let mut verts = HashSet::new();
+        // let inputs = HashSet::from_iter(self.lhs.inputs());
+        // let outputs =
+        //     HashSet::from_iter(self.lhs.outputs());
 
-//         for v in inputs.union(&outputs) {
-//             if verts.contains(v) {
-//                 return false;
-//             }
-//             verts.insert(v);
-//         }
+        // for v in inputs.union(&outputs) {
+        //     if verts.contains(v) {
+        //         return false;
+        //     }
+        //     verts.insert(v);
+        // }
 
-//         true
-//     }
-// }
+        // true
+        unimplemented!()
+    }
+}
